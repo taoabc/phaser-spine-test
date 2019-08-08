5d38cec7762b27ebf706891c947dd42c688399f8
huhaitao <huhaitao@100tal.com>
2019-08-08 20:11:57 +0800

    init

---
+++ build/webpack.dev.js
@@ -0,0 +1,64 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+// TODO not work
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+
+  context: path.resolve(__dirname, '..'),
+
+  entry: {
+    'app': './src/index.ts',
+    // 'dragonbones-phaser.min': path.resolve(__dirname, '../src/index.ts'),
+  },
+
+  resolve: {
+    // Add `.ts` and `.tsx` as a resolvable extension.
+    extensions: ['.ts', '.tsx', '.js'],
+  },
+
+  output: {
+    path: path.resolve(__dirname, '../dist/'),
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: [
+          {
+            loader: 'babel-loader'
+          },
+          {
+            loader: 'ts-loader'
+          }
+        ]
+      },
+      { test: /\.js$/, use: ['babel-loader'] },
+    ]
+  },
+
+  plugins: [
+    new webpack.ProgressPlugin(),
+    new CleanWebpackPlugin({
+      verbose: true
+    }),
+    new HtmlWebpackPlugin({
+      template: './public/template.html',
+    }),
+    new CopyWebpackPlugin([
+      { from: './assets/', to: 'assets/' }
+    ]),
+  ],
+
+  devServer: {
+    // contentBase: `${__dirname}/../dist`,
+    // publicPath: 'Demos/dist/',
+    open: true,
+    compress: true,
+    port: 8456
+  }
+}
